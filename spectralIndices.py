5e3bb8d416ce4b22440bccdfcd0eccb45878c14b
olyna <olympia_g@live.com>
2020-03-18 21:51:18 +0200

    update

--- spectralIndices.py
+++ spectralIndices.py
@@ -56,7 +56,6 @@ def calc_ndvi(nir, red):
     # BE CAREFULL! Without this convertion, doesn't work correctly !
     red = red.astype(np.float32)
     nir = nir.astype(np.float32)
-
     res = (nir-red)/(nir+red)
     # (count, row, col)  Rasterio needs count=1
     res = res.reshape(1, res.shape[0], res.shape[1])
@@ -75,5 +74,68 @@ def calc_ndbi(swir, nir):
 
 def calc_ndwi(nir, swir):
     res = (nir-swir)/nir+swir
+    res = res.reshape(1, res.shape[0], res.shape[1])
+    return res
+
+def calc_evi(nir, red, blue):
+    res = 2.5 * ((nir-red) / (nir + 6*red - 7.5*blue + 1))
+    res = res.reshape(1, res.shape[0], res.shape[1])
+    return res
+
+def calc_ndti(swir1, swir2):
+    swir1 = swir1.astype(np.float32)
+    swir2 = swir2.astype(np.float32)
+    res = (swir1-swir2)/(swir1+swir2)
+    res = res.reshape(1, res.shape[0], res.shape[1])
+    return res
+
+def calc_tvi(nir, red, green):
+    res = 0.5 * (120*(nir-green) - 200*(red-green))
+    res = res.reshape(1, res.shape[0], res.shape[1])
+    return res
+
+def calc_ndvi8a4(nir2, red):
+    red = red.astype(np.float32)
+    nir2 = nir2.astype(np.float32)
+    res = (nir2-red)/(nir2+red)
+    res = res.reshape(1, res.shape[0], res.shape[1])
+    return res
+
+
+def calc_ndvi8a5(nir2, re1):
+    re1 = re1.astype(np.float32)
+    nir2 = nir2.astype(np.float32)
+    res = (nir2-re1)/(nir2+re1)
+    res = res.reshape(1, res.shape[0], res.shape[1])
+    return res
+
+
+def calc_ndvi8a6(nir2, re2):
+    re2 = re2.astype(np.float32)
+    nir2 = nir2.astype(np.float32)
+    res = (nir2-re2)/(nir2+re2)
+    res = res.reshape(1, res.shape[0], res.shape[1])
+    return res
+
+def calc_ndvi65(re2, re1):
+    re1 = re1.astype(np.float32)
+    re2 = re2.astype(np.float32)
+    res = (re2-re1)/(re2+re1)
+    res = res.reshape(1, res.shape[0], res.shape[1])
+    return res
+
+
+def calc_ndvi75(re3, re1):
+    re1 = re1.astype(np.float32)
+    re3 = re3.astype(np.float32)
+    res = (re3-re1)/(re3+re1)
+    res = res.reshape(1, res.shape[0], res.shape[1])
+    return res
+
+
+def calc_ndvi76(re3, re2):
+    re2 = re2.astype(np.float32)
+    re3 = re3.astype(np.float32)
+    res = (re3-re2)/(re3+re2)
     res = res.reshape(1, res.shape[0], res.shape[1])
     return res
